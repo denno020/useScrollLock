58aae825dc81999f67c5d88050bf3df50e9ac086
Luke Denton <denno020@gmail.com>
2021-11-24 23:02:13 +1030

    Add basic scrollbar compensation
    That is, hard coded value

--- src/hooks/useScrollLock.js
+++ src/hooks/useScrollLock.js
@@ -4,11 +4,13 @@ export const useScrollLock = () => {
   const lockScroll = React.useCallback(
     () => {
       document.body.style.overflow = 'hidden';
+      document.body.style.paddingRight = '17px';
     }, [])
 
   const unlockScroll = React.useCallback(
     () => {
       document.body.style.overflow = '';
+      document.body.style.paddingRight = '';
     }, []);
 
   return {
