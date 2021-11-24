5fd72505da61c79303866f49bdf8ccf3c9f308fb
Luke Denton <denno020@gmail.com>
2021-11-24 23:14:43 +1030

    Add dynamic scrollbar compensation

--- src/hooks/useScrollLock.js
+++ src/hooks/useScrollLock.js
@@ -3,8 +3,9 @@ import React from 'react';
 export const useScrollLock = () => {
   const lockScroll = React.useCallback(
     () => {
+      const scrollBarCompensation = window.innerWidth - document.body.offsetWidth;
       document.body.style.overflow = 'hidden';
-      document.body.style.paddingRight = '17px';
+      document.body.style.paddingRight = `${scrollBarCompensation}px`;
     }, [])
 
   const unlockScroll = React.useCallback(
