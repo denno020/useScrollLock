e523111bac8443d08facd258f64e6200e193507f
Luke Denton <denno020@gmail.com>
2021-11-25 23:40:41 +1030

    Set up scrolling contexts demonstration

--- src/components/PLP.jsx
+++ src/components/PLP.jsx
@@ -47,12 +47,12 @@ const PLP = () => {
 
             <strong>Description</strong>
 
-            <div>
+            <div className="product-card__description">
               'Twas the night before Christmas, when all through the house
               Not a creature was stirring, not even a mouse;
               The stockings were hung by the chimney with care,
               In hopes that St. Nicholas soon would be there;
-<br/><br/>
+              <br/><br/>
               The children were nestled all snug in their beds,
               While visions of sugar-plums danced in their heads;
               And mamma in her 'kerchief, and I in my cap,
@@ -62,6 +62,21 @@ const PLP = () => {
               I sprang from the bed to see what was the matter.
               Away to the window I flew like a flash,
               Tore open the shutters and threw up the sash.
+              <br/><br/>
+              The moon on the breast of the new-fallen snow
+              Gave the lustre of mid-day to objects below,
+              When, what to my wondering eyes should appear,
+              But a miniature sleigh, and eight tiny reindeer,
+              <br/><br/>
+              With a little old driver, so lively and quick,
+              I knew in a moment it must be St. Nick.
+              More rapid than eagles his coursers they came,
+              And he whistled, and shouted, and called them by name;
+              <br/><br/>
+              "Now, DASHER! now, DANCER! now, PRANCER and VIXEN!
+              On, COMET! on CUPID! on, DONNER and BLITZEN!
+              To the top of the porch! to the top of the wall!
+              Now dash away! dash away! dash away all!"
             </div>
           </div>
         </div>
